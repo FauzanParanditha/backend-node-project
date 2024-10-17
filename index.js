a2fd152f06dfe558816f39e7616c6c1dfcb4a619
Fauzan <fauzan@fellow.pandi.id>
2024-10-17 11:27:36 +0700

    feat: find me

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import authRouter from "./routers/authRouter.js";
 import userRouter from "./routers/users/userRouter.js";
+import Admin from "./models/adminModel.js";
+import { jwtMiddlewareAdmin } from "./middlewares/admin_jwt.js";
 
 dotenv.config();
 
@@ -24,6 +26,31 @@ app.use("/auth", userRouter);
 app.get("/", (req, res) => {
   res.json({
     message: "Hello World!",
+  });
+});
+
+app.get("/me", jwtMiddlewareAdmin, async (req, res) => {
+  const { adminId, verified } = req.admin;
+  if (!adminId) {
+    return res.status(400).json({
+      success: false,
+      code: 400,
+      message: "admin id is not provide",
+    });
+  }
+  const existAdmin = await Admin.findOne({ _id: adminId });
+  if (!existAdmin) {
+    return res.status(401).json({
+      success: false,
+      code: 401,
+      message: "admin is not exist!",
+    });
+  }
+  res.status(200).json({
+    success: true,
+    code: 200,
+    message: "me",
+    data: existAdmin,
   });
 });
 
