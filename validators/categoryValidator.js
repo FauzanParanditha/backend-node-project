d9af24b0018a80eb1200c6b01eefc03f1a2b15fe
Fauzan <fauzan@fellow.pandi.id>
2024-10-21 16:04:35 +0700

    chore: add trim

--- validators/categoryValidator.js
+++ validators/categoryValidator.js
@@ -1,6 +1,6 @@
 import joi from "joi";
 
 export const categorySchema = joi.object({
-  name: joi.string().min(1).max(10).required(),
+  name: joi.string().min(1).max(10).trim().required(),
   adminId: joi.string().required(),
 });
