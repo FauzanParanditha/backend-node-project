90c7a492633eba4d31e1f0acca04db2984c117ae
Fauzan <fauzan@fellow.pandi.id>
2024-10-31 10:18:30 +0700

    feat: qris

---
+++ controllers/qrisController.js
@@ -0,0 +1,143 @@
+import uuid4 from "uuid4";
+import User from "../models/userModel.js";
+import { calculateTotal, validateOrderProducts } from "../utils/helper.js";
+import {
+  createSignature,
+  generateMerchantTradeNo,
+  generateRequestId,
+  generateTimestamp,
+  merchantId,
+  paylabsApiUrl,
+} from "../utils/paylabs.js";
+import { orderSchema } from "../validators/orderValidator.js";
+import { validateQrisRequest } from "../validators/paymentValidator.js";
+import axios from "axios";
+import Order from "../models/orderModel.js";
+
+export const createQris = async (req, res) => {
+  try {
+    const validatedProduct = await orderSchema.validateAsync(req.body, {
+      abortEarly: false,
+    });
+
+    const existUser = await User.findById(validatedProduct.userId);
+    if (!existUser) {
+      return res.status(404).json({
+        success: false,
+        message: "user does not exist!",
+      });
+    }
+
+    const products = await validateOrderProducts(validatedProduct.products);
+    if (!products.length) {
+      return res.status(404).json({
+        success: false,
+        message: "No valid products found to create the order",
+      });
+    }
+
+    const requestBodyForm = {
+      orderId: uuid4(),
+      userId: validatedProduct.userId,
+      products,
+      totalAmount: calculateTotal(products),
+      phoneNumber: validatedProduct.phoneNumber,
+      paymentStatus: "pending",
+      paymentMethod: validatedProduct.paymentMethod,
+      storeId: validatedProduct.storeId,
+    };
+
+    const timestamp = generateTimestamp();
+    const requestId = generateRequestId();
+    const merchantTradeNo = generateMerchantTradeNo();
+
+    const requestBody = {
+      requestId,
+      merchantId,
+      storeId: requestBodyForm.storeId,
+      paymentType: "QRIS",
+      amount: requestBodyForm.totalAmount,
+      merchantTradeNo,
+      notifyUrl: "http://103.122.34.186:5000/api/order/webhook/paylabs",
+      expire: 300,
+      feeType: "OUR",
+      productName: requestBodyForm.products.map((p) => p.title).join(", "),
+      productInfo: requestBodyForm.products.map((product) => ({
+        id: product.productId.toString(),
+        name: product.title,
+        price: product.price,
+        type: product.category,
+        quantity: product.quantity,
+      })),
+    };
+
+    // Validate request body
+    const { error } = validateQrisRequest(requestBody);
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        errors: error.details.map((err) => err.message),
+      });
+    }
+
+    // Generate request signature
+    const signature = createSignature(
+      "POST",
+      "/payment/v2.1/qris/create",
+      requestBody,
+      timestamp
+    );
+
+    // Configure headers
+    const headers = {
+      "Content-Type": "application/json;charset=utf-8",
+      "X-TIMESTAMP": timestamp,
+      "X-SIGNATURE": signature,
+      "X-PARTNER-ID": merchantId,
+      "X-REQUEST-ID": requestId,
+    };
+
+    // console.log(requestBody);
+    // console.log(headers);
+
+    // Make API request to Paylabs
+    const response = await axios.post(
+      `${paylabsApiUrl}/payment/v2.1/qris/create`,
+      requestBody,
+      { headers }
+    );
+
+    console.log("Response:", response.data);
+    if (!response.data) {
+      return res.status(400).json({
+        success: false,
+        message: "failed to create payment",
+      });
+    }
+
+    if (
+      response.data.errCode != 0 &&
+      requestBodyForm.paymentMethod === "paylabs"
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "error, " + response.data.errCode,
+      });
+    }
+
+    const savedOrder = await Order.create({ ...data, ...paymentLink });
+    res.status(200).json({
+      success: true,
+      paymentLink: response.data.url,
+      paymentId: response.data.merchantTradeNo,
+      storeId: response.data.storeId,
+      orderId: savedOrder._id,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "An error occurred",
+      error: error.message,
+    });
+  }
+};
