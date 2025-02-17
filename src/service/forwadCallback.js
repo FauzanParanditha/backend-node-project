e3ed4346ab5e92e9f97934ad4aaaca1c8b2c8c08
Fauzan Paranditha <paranditha@gmail.com>
2025-02-17 13:34:56 +0700

    fix: error message

--- src/service/forwadCallback.js
+++ src/service/forwadCallback.js
@@ -125,10 +125,8 @@ export const forwardCallback = async ({ payload, retryCount = 0 }) => {
                 logger.info(`Callback successfully forwarded on attempt ${retryCount + 1}`);
                 return true; // Exit loop on success
             } catch (err) {
-                logger.error(
-                    `Attempt ${retryCount + 1} failed: ${err instanceof AggregateError ? JSON.stringify(err.errors) : err.message}`,
-                );
-                logger.error(`Full error object: ${JSON.stringify(err, Object.getOwnPropertyNames(err))}`);
+                logger.error(`Attempt ${retryCount + 1} failed: ${err.message}`);
+                // logger.error(`Full error object: ${JSON.stringify(err, Object.getOwnPropertyNames(err))}`);
 
                 retryCount++;
                 if (retryCount < retryIntervals.length) {
