45ae03bb50dc623f0cde3c0ee650d712eb1b6232
Fauzan Paranditha <paranditha@gmail.com>
2025-03-13 11:41:33 +0700

    fix: env

--- test/test-rmq.js
+++ test/test-rmq.js
@@ -1,4 +1,7 @@
 import amqplib from "amqplib";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const RABBITMQ_URL = process.env.RABBITMQ_URL || "amqp://localhost"; // Sesuaikan jika pakai Docker: amqp://guest:guest@localhost
 
