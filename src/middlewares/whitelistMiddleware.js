5dae22d751d363e8b1bf1c8cea4a9c1d426fe806
Fauzan Paranditha <paranditha@gmail.com>
2025-10-16 15:34:01 +0700

    debug: logger

--- src/middlewares/whitelistMiddleware.js
+++ src/middlewares/whitelistMiddleware.js
@@ -3,7 +3,8 @@ import IPWhitelist from "../models/ipWhitelistModel.js";
 
 export const whitelistMiddlewareVerify = async (req, res, next) => {
     const clientIP = req.headers["x-forwarded-for"] || req.ip;
-    console.log(clientIP);
+    // console.log(clientIP);
+    logger.info(`Client IP: ${clientIP}`);
 
     try {
         const whitelistedIP = await IPWhitelist.findOne({ ipAddress: clientIP });
